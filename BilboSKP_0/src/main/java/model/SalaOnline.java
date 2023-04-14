9fcfc045666efbec881641773cb68793ba47097e
Rivo <107114580+Riv0J@users.noreply.github.com>
2023-04-14 20:49:04 +0200

    Update SalaOnline.java

--- BilboSKP_0/src/main/java/model/SalaOnline.java
+++ BilboSKP_0/src/main/java/model/SalaOnline.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class SalaOnline extends Sala{
 	//este hashmap contendrá todas las salas online disponibles
-	private static HashMap<String, Sala> salasOnlineCargadas = new HashMap<String, Sala>();
+	private static HashMap<String, SalaOnline> salasOnlineCargadas = new HashMap<String, SalaOnline>();
 
 	public SalaOnline(String idSala, String nombre, String dificultad, String tematica, String descripcion,
 			int tiempoMax, int jugadoresMin, int jugadoresMax, int edad_recomendada) {
@@ -12,11 +12,14 @@ public class SalaOnline extends Sala{
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void setSalasCargadas(HashMap<String, Sala> salasCargadas) {
+	public static void setSalasCargadas(HashMap<String, SalaOnline> salasCargadas) {
 		salasOnlineCargadas = salasCargadas;
 	}
-	public static HashMap<String, Sala> getSalasCargadas() {
+	public static HashMap<String, SalaOnline> getSalasCargadas() {
 		return salasOnlineCargadas;
+	}
+	public static SalaOnline getSalaPorId(String idSala) {
+		return salasOnlineCargadas.get(idSala);
 	}
 	public static void clearSalasCargadas() {
 		salasOnlineCargadas.clear();
