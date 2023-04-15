969230f3f0546b0ccc0c51fcdd1f5ef3188e534e
Rivo <107114580+Riv0J@users.noreply.github.com>
2023-04-15 22:13:25 +0200

    Update Jugador.java

--- BilboSKP_0/src/main/java/model/Jugador.java
+++ BilboSKP_0/src/main/java/model/Jugador.java
@@ -2,18 +2,12 @@ package model;
 
 public class Jugador {
     private String alias;
-    private boolean esSuscriptor;
-
-    public Jugador(String alias, boolean esSuscriptor)  {
+    public Jugador(String alias)  {
         this.alias = alias;
-        this.esSuscriptor = esSuscriptor;
+
     }
 
     public String getAlias() {
         return alias;
-    }
-
-    public boolean esSuscriptor() {
-    	return this.esSuscriptor;
     }
 }
