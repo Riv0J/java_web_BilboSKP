97883e4237083461a1e357b80be42d04cad1e80c
Rivo <107114580+Riv0J@users.noreply.github.com>
2023-04-15 23:38:58 +0200

    Update SalaOnline.java

--- BilboSKP_0/src/main/java/model/SalaOnline.java
+++ BilboSKP_0/src/main/java/model/SalaOnline.java
@@ -18,8 +18,8 @@ public class SalaOnline extends Sala{
 	public static HashMap<Integer, SalaOnline> getSalasCargadas() {
 		return salasOnlineCargadas;
 	}
-	public static SalaOnline getSalaPorId(int i) {
-		return salasOnlineCargadas.get(i);
+	public static SalaOnline getSalaPorId(int idSalaOnline) {
+		return salasOnlineCargadas.get(idSalaOnline);
 	}
 	public static void clearSalasCargadas() {
 		salasOnlineCargadas.clear();
