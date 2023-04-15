6e145341bbd166187be5e596cc2c725ef7801025
Rivo <107114580+Riv0J@users.noreply.github.com>
2023-04-16 00:33:10 +0200

    Update App.java

--- BilboSKP_0/src/main/java/view/App.java
+++ BilboSKP_0/src/main/java/view/App.java
@@ -1,14 +1,25 @@
 package view;
 
+import java.util.Date;
+
 import control.BilboSKP;
+import model.PartidaOnline;
+import model.SalaOnline;
+import model.Suscriptor;
 
 public class App {
 
 	public static void main(String[] args) {
 		try {
 			BilboSKP.cargarSalasOnline();
+			BilboSKP.crearSuscripcion("admin", "admin", 7, "administrador", "Bilbo", "SKP", "2023-10-10");
+			Suscriptor sus = BilboSKP.loginSuscriptor("admin", "admin");
+			if (sus!=null) {
+				PartidaOnline PO = new PartidaOnline(SalaOnline.getSalaPorId(1),sus,4,"PRESTOS");
+				BilboSKP.guardarPartidaOnline(PO);
+			}
+			
 		} catch (Throwable e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
