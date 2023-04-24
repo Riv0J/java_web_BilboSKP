631a1eef3756938a91acbf83a37e0fd33767a398
UrkoDun <122871999+UrkoDun@users.noreply.github.com>
2023-04-24 13:46:57 +0200

    frases

---
+++ main/java/view/Frases.java
@@ -0,0 +1,89 @@
+//modelo copiado de FUICA con idea de poner aqui las traducciones y todos los textos que hirian en la web
+package view;
+
+import java.util.HashMap;
+
+public class Frases {
+	
+	private static HashMap<String,String> frases ;
+	
+	public static void rellenarFrases() {
+		frases=new HashMap<String, String>();
+		/*HEADER*/
+		frases.put("ESheaderSalas", "Salas de escape");
+		frases.put("ENheaderSalas", "Escape rooms");
+		frases.put("ESheaderRanking", "Ranking");
+		frases.put("ENheaderRanking", "Ranking");
+		frases.put("ESheaderUnirse", "Unirse a partida");
+		frases.put("ENheaderUnirse", "Join a game");
+		frases.put("ESheaderContacto", "Contacto");
+		frases.put("ENheaderContacto", "Contact us");
+		/*FAQ+Contacto*/
+		frases.put("ESContactocontacto", "Contacto");
+		frases.put("ENContactocontacto", "Contact");
+		frases.put("ESContactoDireccion", "Dirección");
+		frases.put("ENContactoDireccion", "Address");
+		frases.put("ESContactoBotonCerrar", "Cerrar mapa");
+		frases.put("ENContactoBotonCerrar", "Close map");
+		frases.put("ESContactoTelefono", "Telefono");
+		frases.put("ENContactoTelefono", "Phone number");
+		frases.put("ESContactoRedes", "Redes Sociales");
+		frases.put("ENContactoRedes", "Social Media");
+		frases.put("ESContactoFAQ", "Preguntas frecuentes");
+		frases.put("ENContactoFAQ", "FAQ");
+		frases.put("ESContactoFAQ1", "¿Como comprar cupones?");
+		frases.put("ENContactoFAQ1", "How I buy coupons");
+		frases.put("ESContactoFAQ1R", "Quería un perro pero borracho compró un caballo en una subasta online: El joven de 24 años vio que había un caballo muy barato, y quiso subir su valor para que alguien termine pagando más por el animal. Así fue que sin recordar mucho, debido al alcohol, acabó por recibir un llamado al día siguiente donde le preguntaban cuándo iba a buscar el equino.");
+		frases.put("ENContactoFAQ1R", "Lorem ipsum ipsum lorem");
+		frases.put("ESContactoFAQ2", "¿Como organizo una partida?");
+		frases.put("ENContactoFAQ2", "How I organice a game?");
+		frases.put("ESContactoFAQ2R", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+		frases.put("ENContactoFAQ2R", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+		frases.put("ESContactoFAQ3", "¿Cuanto tarda en caducar un cupón?");
+		frases.put("ENContactoFAQ3", "How many time takes to a coupon to expire?");
+		frases.put("ESContactoFAQ3R", "Los yogures no tienen fecha de caducidad. Y esto es así desde hace años. En concreto, desde 2014, cuando el entonces Ministerio de Agricultura, Alimentación y Medio Ambiente (MAGRAMA)estableció que este alimento solo debería indicar una fecha de consumo preferente (no de caducidad), determinada por los fabricantes. La medida, recogida en el Real Decreto 271/2014,pretendía simplificar el etiquetado, adaptar la legislación española a la europea y reducir el desperdicio alimentario.");
+		frases.put("ENContactoFAQ3R", "Lorem ipsum de yogures");
+		frases.put("ESContactoFAQ4", "¿Que pasa si se termina el tiempo en una sala?");
+		frases.put("ENContactoFAQ4", "What happens when time runs out?");
+		frases.put("ESContactoFAQ4R", "Lorem ipsum");
+		frases.put("ENContactoFAQ4R", "Lorem ipsum ipidfjaszhoshfohjdoshfj");
+		frases.put("ESContactoFAQ5", "¿Como sé cuan dificil es una sala?");
+		frases.put("ENContactoFAQ5", "Can I know how hard a escape room is?");
+		frases.put("ESContactoFAQ5R", "El Bosque Petrificado de Puyango es un yacimiento fosilífero del tipo conocido popularmente como «bosque petrificado» situado en la cuenca media del río Puyango, entre las provincias ecuatorianas de El Oro y Loja. El área cubre 2,658 hectáreas y sus sedimentos están divididos en cuatro formaciones geológicas (Formación Zapotillo, Formación Ciano, Formación Ambin y Formación Progreso), que datan de finales del período Cretácico.");
+		frases.put("ENContactoFAQ5R", "The Puyango Petrified Forest is a fossil deposit of the type popularly known as \"petrified forest\" located in the middle basin of the Puyango River, between the Ecuadorian provinces of El Oro and Loja. The area covers 2,658 hectares and its sediments are divided into four geological formations (Zapotillo Formation, Ciano Formation, Ambin Formation and Progreso Formation), dating from the late Cretaceous period.");
+		frases.put("ESContactoOtras", "Otras consultas de utilidad");
+		frases.put("ENContactoOtras", "Other utilities");
+		frases.put("ESContactoParking", "Vienes en coche y quieres encontrar un PARKING?");
+		frases.put("ENContactoParking", "Are you coming by car and looking for a PARKING spot?");
+		frases.put("ESContactoMetro", "¿Las paradas de Metro mas cercanas? ");
+		frases.put("ENContactoMetro", "The nearest subway stations?");
+		frases.put("ESContactoParking", "Vienes en coche y quieres encontrar un PARKING?");
+		frases.put("ENContactoParking", "Are you coming by car and looking for a PARKING spot?");
+		frases.put("ESContactoOtraConsulta", "¿Tienes alguna otra consulta que hacernos?");
+		frases.put("ENContactoOtraConsulta", "Do you have any other question to ask us?");
+		frases.put("ESContactoTextarea", "Escribe aqui tu pregunta");
+		frases.put("ENContactoTextarea", "Write your question here");
+		frases.put("ESContactoEnviar", "Enviar");
+		frases.put("ENContactoEnviar", "Send");
+		frases.put("ESContactoBorrar", "Borrar");
+		frases.put("ENContactoBorrar", "Erase");
+		/**/
+		
+		
+
+		
+		
+		frases.put("pruebalupa", "Habéis conseguido resolver la prueba, id al jardín mostrado en el MAPA");
+		frases.put("pruebalinterna", "Habéis conseguido resolver la prueba, ahora vamos a unir estos pedazos de papel");
+		frases.put("pruebaletras_solucion", "El ultimo ritual sera en la mansion");
+		frases.put("pruebaletras_resuelto", "Habéis conseguido descifrar una ubicación  que aparece en el libro \n¿Será la MANSIÓN de Dr.Doofenshmirtz \ndonde podamos resolver este caso?");
+		frases.put("pruebacodigo_resuelto", "Conseguís introducir el código correcto    para abrir la caja fuerte; se abre haciendo  un chirrido metálico horrible.\nSorprendidos, descubrís que la caja está   vacía... salvo por una ¡LLAVE dorada!.     La recogéis, con la certeza de que pronto  la necesitaréis");
+		
+	}
+	
+	public static String getFrase(String claveFrase) {
+		return frases.get(claveFrase);
+	}
+	
+	}
+
