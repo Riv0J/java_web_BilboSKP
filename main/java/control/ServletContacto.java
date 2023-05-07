9ad7062c239327d26fcdabd783a4fd15c24d0401
pauct04 <122865298+pauct04@users.noreply.github.com>
2023-05-07 19:30:32 +0200

    seccion contacto en proceso

---
+++ main/java/control/ServletContacto.java
@@ -0,0 +1,35 @@
+package control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/contacto")
+public class ServletContacto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		System.out.println("Doget tienda");
+		String urlPrevia = "./Contacto";
+		System.out.println("ServletContacto urlprevia establecida = " + urlPrevia);
+		HttpSession sesion = request.getSession();
+		sesion.setAttribute("urlPrevia", urlPrevia);	
+
+		// Enviar la respuesta al usuario
+		request.getRequestDispatcher("index.jsp?sec=contacto").forward(request, response);
+
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
